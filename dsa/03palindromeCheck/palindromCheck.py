1345d61e01e5b36de505779441fa80cacfc23d56
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-07-31 21:40:13 +0530

    add dsa

---
+++ dsa/03palindromeCheck/palindromCheck.py
@@ -0,0 +1,35 @@
+n = 1234
+# n= 12321
+
+def palindromCheck(num):
+    n = num
+    result = 0
+    prvNum =0
+    while n > 0:
+        lastDigit = n%10
+        result = prvNum *10 + lastDigit
+        prvNum = result
+        n = n//10
+
+    if result == num:
+        print(f"given {num} is palindrome")
+    else:
+        print(f"given {num} is not palindrome")
+    
+# palindromCheck(n)
+
+def modifiedCodePalindrome(num):
+    n = num
+    result = 0
+
+    while n > 0: 
+        lastDigit = n % 10
+        result = result *10 + lastDigit
+        n = n//10
+    
+    if result == num:
+        print(f"{num} is palindrom")
+    else:
+        print(f"{num} is NOT palindrom")
+
+modifiedCodePalindrome(n)
---
+++ dsa/04armstrongNumber/README.md
@@ -0,0 +1,24 @@
+## What is Armstrong Number
+
+**example 1**: 
+
+153= 1^3 + 5^3 + 3^3
+
+= 1 + 125 + 27
+
+= 153
+***
+
+**example 2**:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+
+=1+ 1296 + 81 + 256 
+
+= 1634
+
+***
+
+> from above 3 is the length of 153
+>
+> from above 4 is the length of 1634
