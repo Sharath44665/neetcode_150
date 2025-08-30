ed6210755b671166da45b452ae34d5b7e43d81e6
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-08-30 21:28:29 +0530

    add armstrong solution

---
+++ dsa/04armstrongNumber/armstrongNumber.py
@@ -0,0 +1,52 @@
+def armStrongNumber(num):
+# time = o (k * log to the base 10 N ) where k is length of number
+# space is constant
+
+    # checking the length of number
+    n= num
+    length = 0
+    while  n> 0:
+        length += 1
+        n = n//10
+
+    n= num 
+    sum = 0
+    
+    while n > 0: # 153
+        lastDigit = n%10
+        newLength = length
+        multiplyResult = 1
+        while newLength > 0:
+            multiplyResult= multiplyResult * lastDigit
+            newLength -= 1
+        sum = sum+ multiplyResult
+        n = n//10
+
+    if sum == num: 
+        print(f"{num} is ArmstrongNumber")
+    else:
+        print(f"{num} is NOT ArmstrongNumber")
+        
+# n= 1634
+# armStrongNumber(n)
+
+def armStrongNumberTwo(num):
+    # o(k) where k is the length of num
+    n= num
+    length = 0
+    while  n> 0:
+        length += 1
+        n = n//10
+
+    sum =0
+    power= length
+    while length > 0:
+        lastDigit = n % 10
+        sum = sum + (lastDigit ** power)
+        print(sum)
+        length -= 1
+
+    print(sum)
+
+n= 1634
+armStrongNumberTwo(n)
