806e3beff414544770f71830233177dcf2e55c45
Sharath44665 <sharathwelcomesyouin@gmail.com>
2026-02-26 01:12:47 +0530

    update right rotate arr

--- dsa/25Right_rotate_arr_k_places/README.md
+++ dsa/25Right_rotate_arr_k_places/README.md
@@ -1,3 +1,15 @@
 # Right rotate array k times
 
 https://leetcode.com/problems/rotate-array/?envType=problem-list-v2&envId=two-pointers&difficulty=MEDIUM
+
+hints:
+
+example arr = [1, 2, 3, 4, 5, 6, 7] and k = 3
+target arr = [5, 6, 7, 1, 2, 3, 4]
+
+1. first reverse the whole array = [7, 6, 5, 4, 3, 2, 1]
+
+2. now reverse 0 to k-1 = [5, 6, 7, 4, 3, 2, 1]
+
+3. now reverse k to len(arr)-1 = [5, 6, 7, 1, 2, 3, 4] which is our target arr
+
--- dsa/25Right_rotate_arr_k_places/bruteForce.py
+++ dsa/25Right_rotate_arr_k_places/bruteForce.py
@@ -31,6 +31,29 @@ class Solution(object):
             startIdx += 1
         pass
     
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        firstPart = []
+        secondPart = []
 
+        for idx in range(len(nums)-k, len(nums)):
+            firstPart.append(nums[idx])
+
+        for idx in range(0, len(nums)-k):
+            secondPart.append(nums[idx])
+        
+        sidx = 0
+        for idx in range(len(nums)):
+            if idx < len(firstPart):
+                nums[idx] =firstPart[idx]
+            else: 
+                nums[idx] = secondPart[sidx]
+                sidx += 1
 
         
